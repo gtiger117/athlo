f4ade2112bbe6dc9d7f6e5a24090fcf64e47dc29
gtiger117 <104471486+gtiger117@users.noreply.github.com>
2023-10-18 00:18:05 +0900

    2

--- src/Athlo.php
+++ src/Athlo.php
@@ -34,7 +34,7 @@ class Athlo extends Tool
                 if(strpos($submenu[$k]->name,"\\")){
                     $tempMenu =  MenuItem::resource($submenu[$k]->name);
                 }else{
-                    $tempMenu =  MenuItem::resource("App\\Nova\\".$submenu[$k]->name);
+                    $tempMenu =  MenuItem::resource('\\Gtiger117\\Athlo\\Nova\\'.$submenu[$k]->name);
                 }
                 
             }else{
@@ -48,7 +48,22 @@ class Athlo extends Tool
     public function boot()
     {
         Nova::resources([
+            \Gtiger117\Athlo\Nova\GiftVoucher::class,
+            \Gtiger117\Athlo\Nova\VoucherOrder::class,
+            \Gtiger117\Athlo\Nova\PaymentMethodType::class,
+            \Gtiger117\Athlo\Nova\PaymentGateway::class,
+            \Gtiger117\Athlo\Nova\VoucherEmailTemplate::class,
+            \Gtiger117\Athlo\Nova\ShippingMethod::class,
+            \Gtiger117\Athlo\Nova\ShippingMethodType::class,
+            \Gtiger117\Athlo\Nova\PromotionalVoucher::class,
+            \Gtiger117\Athlo\Nova\Pickup::class,
+            \Gtiger117\Athlo\Nova\PickupGroup::class,
+            \Gtiger117\Athlo\Nova\Region::class,
             \Gtiger117\Athlo\Nova\Banner::class,
+            \Gtiger117\Athlo\Nova\Blog::class,
+            \Gtiger117\Athlo\Nova\Tax::class,
+            \Gtiger117\Athlo\Nova\Theme::class,
+            \Gtiger117\Athlo\Nova\Package::class,
         ]);
         Nova::script('athlo', __DIR__.'/../dist/js/tool.js');
         Nova::style('athlo', __DIR__.'/../dist/css/tool.css');
@@ -62,21 +77,72 @@ class Athlo extends Tool
      */
     public function menu(Request $request)
     {
-        // $menus = [];
-        // array_push($menus,MenuSection::dashboard(Main::class)->icon('chart-bar'));
-        // $result = DB::table('nova_menu_menu_items')->where('parent_id',null)->orderBy('order', 'asc')->get();
-        // for($i=0;$i<count($result);$i++){
-        //     if(DB::table('nova_menu_menu_items')->where('parent_id',$result[$i]->id)->exists()){
-        //         $tempMenu =  MenuSection::make($result[$i]->name, $this->get_menus($result[$i]->id))->collapsable()->collapsedByDefault();
-        //     }else if($result[$i]->value == null || $result[$i]->value == ""){
-        //         $tempMenu =  MenuItem::resource($result[$i]->name);
-        //     }else{
-        //         $tempMenu =  MenuItem::link($result[$i]->name, $result[$i]->value);
-        //     }
-        //     array_push($menus,$tempMenu);
-        // }
-        // return $menus;
-        return MenuItem::resource(\Gtiger117\Athlo\Nova\Banner::class);
+        $menus = [];
+        array_push($menus,MenuSection::dashboard(Main::class)->icon('chart-bar'));
+        $result = DB::table('nova_menu_menu_items')->where('parent_id',null)->orderBy('order', 'asc')->get();
+        for($i=0;$i<count($result);$i++){
+            if(DB::table('nova_menu_menu_items')->where('parent_id',$result[$i]->id)->exists()){
+                $tempMenu =  MenuSection::make($result[$i]->name, $this->get_menus($result[$i]->id))->collapsable()->collapsedByDefault();
+            }else if($result[$i]->value == null || $result[$i]->value == ""){
+                $tempMenu =  MenuItem::resource('\\Gtiger117\\Athlo\\Nova\\'.$result[$i]->name);
+            }else{
+                $tempMenu =  MenuItem::link($result[$i]->name, $result[$i]->value);
+            }
+            array_push($menus,$tempMenu);
+        }
+        return $menus;
+        // $menus = [
+        //     MenuSection::make('Product Catalogue', [
+        //         MenuItem::resource(\Gtiger117\Athlo\Nova\GiftVoucher::class),
+        //         MenuGroup::make('Settings', [
+        //             // MenuItem::resource(PaymentMethod::class),
+        //             // MenuItem::resource(PaymentMethodType::class),
+        //             // MenuItem::resource(PaymentGateway::class),
+        //         ])->collapsable()->collapsedByDefault(),
+        //     ])->icon('briefcase')->collapsable()->collapsedByDefault(),
+
+        //     MenuSection::make('Ordering', [
+        //         MenuItem::resource(\Gtiger117\Athlo\Nova\VoucherOrder::class),
+        //         MenuGroup::make('Settings', [
+        //             // MenuItem::resource(PaymentMethod::class),
+        //             MenuItem::resource(\Gtiger117\Athlo\Nova\PaymentMethodType::class),
+        //             MenuItem::resource(\Gtiger117\Athlo\Nova\PaymentGateway::class),
+        //             MenuItem::resource(\Gtiger117\Athlo\Nova\VoucherEmailTemplate::class),
+        //         ])->collapsable()->collapsedByDefault(),
+        //     ])->icon('briefcase')->collapsable()->collapsedByDefault(),
+
+        //     MenuSection::make('Marketing', [
+        //         MenuItem::resource(\Gtiger117\Athlo\Nova\PromotionalVoucher::class),
+        //         // MenuItem::resource(ShippingMethodType::class),
+        //         MenuGroup::make('Settings', [
+        //             // MenuItem::resource(Pickup::class),
+        //             // MenuItem::resource(PickupGroup::class),
+        //             // MenuItem::resource(Region::class),
+        //         ])->collapsable()->collapsedByDefault(),
+        //     ])->icon('briefcase')->collapsable()->collapsedByDefault(),
+
+        //     MenuSection::make('Shipping', [
+        //         MenuItem::resource(\Gtiger117\Athlo\Nova\ShippingMethod::class),
+        //         MenuItem::resource(\Gtiger117\Athlo\Nova\ShippingMethodType::class),
+        //         MenuGroup::make('Settings', [
+        //             MenuItem::resource(\Gtiger117\Athlo\Nova\Pickup::class),
+        //             MenuItem::resource(\Gtiger117\Athlo\Nova\PickupGroup::class),
+        //             MenuItem::resource(\Gtiger117\Athlo\Nova\Region::class),
+        //         ])->collapsable()->collapsedByDefault(),
+        //     ])->icon('briefcase')->collapsable()->collapsedByDefault(),
+
+        //     MenuSection::make('Website', [
+        //         // MenuItem::resource(Page::class),
+        //          MenuItem::link('Page', '/page'),  
+        //          MenuItem::resource(\Gtiger117\Athlo\Nova\Banner::class),                  
+        //          MenuItem::resource(\Gtiger117\Athlo\Nova\Blog::class),                  
+        //      ])->icon('trash')->collapsable()->collapsedByDefault(),
+
+        //      MenuSection::make('Settings', [
+        //         MenuItem::resource(\Gtiger117\Athlo\Nova\Tax::class),
+        //     ])->icon('cog')->collapsable()->collapsedByDefault(),
+        // ];
+        return $menus;
     }
     
 }
